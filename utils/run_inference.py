4ebe659717cc14d71bed83c93ffbc4f14963c986
tdhooghe <thomas.dhooghe95@gmail.com>
2022-02-23 21:06:47 +0100

    add cuda and inference check

---
+++ utils/run_inference.py
@@ -0,0 +1,19 @@
+#%%
+import torch
+import os
+
+print(os.getcwd())
+#%%
+# Model
+model = torch.hub.load('ultralytics/yolov5', 'yolov5n', device='cpu')  # or yolov5m, yolov5l, yolov5x, custom
+
+
+# Images
+img = f'{os.getcwd()}/data/images/zidane.jpg'  # or file, Path, PIL, OpenCV, numpy, list
+#img = '/mnt/nas/private/photos/vakanties/2013 - Cavalaire sur Mer (St. Tropez)/DSC00003.JPG'
+# Inference
+results = model(img)
+
+# Results
+results.print()  # or .show(), .save(), .crop(), .pandas(), etc.
+results.save()
