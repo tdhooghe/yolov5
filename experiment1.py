b682bd98cb3fd1b15612e4e3c4ca036d23602a5f
Thomas d´Hooghe <thomas.dhooghe95@gmail.com>
2022-05-07 15:04:53 +0200

    experiment1 small naming changes

--- experiment1.py
+++ experiment1.py
@@ -2,31 +2,31 @@ from detect import run
 from datetime import datetime
 import pandas as pd
 
-MODELS = ["yolov5n", "yolov5s", "yolov5m", "yolov5l", "yolov5n6", "yolov5s6",
-          "yolov5m6", "yolov5l6"]
+MODELS = [#"yolov5n", "yolov5s", "yolov5m", "yolov5l"
+          "yolov5n6", "yolov5s6", "yolov5m6", #"yolov5l6"
+          ]
 
 PRECISION = ['int8', 'fp16', 'fp32']
 
 
 def run_experiment1():
-    column_names = ["model", "img_size", "precision", "prep_time", "NMS_time", "latency", "inference_time",
+    column_names = ["model", "precision", "prep_time", "NMS_time", "latency", "inference_time",
                     "total_time",
                     "FPS", "experiment_time"]
-    exp2_df_results = pd.DataFrame(columns=column_names)
+    exp1_df_results = pd.DataFrame(columns=column_names)
 
     counter = 0
     for model in MODELS:
         imgsize = 1280 if '6' in model else 640
         for precision in PRECISION:
-            # for batch_size in BATCH_SIZES:
             start_experiment = datetime.now()
-            row = [model, imgsize, precision]
+            row = [model, precision]
             print(row)
             temp = run(
                 weights=model + '_openvino_model_' + precision,
-                source="../datasets/coco/images/exp2/",  # 000000463199.jpg
-                imgsz=(imgsize, imgsize),
+                source="../datasets/coco/images/val2017",  # 000000463199.jpg
                 nosave=True,
+                imgsz=(imgsize, imgsize)
             )
             row.append(temp[0])  # preprocessing
             row.append(temp[2])  # NMS
@@ -36,19 +36,16 @@ def run_experiment1():
             row.append(1 / (sum(temp)) * 1E3)  # FPS
             row.append((datetime.now() - start_experiment).seconds)  # duration of experiment
             print(row)
-            exp2_df_results.loc[counter] = row
+            exp1_df_results.loc[counter] = row
             counter += 1
-
-    print(exp2_df_results)
+    print(exp1_df_results)
     # store results
-    filename = f'exp2_results/exp2_df_results_{datetime.now().strftime("%d-%m-%Y_%H-%M")}'
-    exp2_df_results.round(3)
-    print(exp2_df_results)
-    exp2_df_results.to_pickle(filename + '.pkl')
-    exp2_df_results.to_csv(filename + '.csv')
+    filename = f'exp1_results/exp1_df_results_{datetime.now().strftime("%d-%m-%Y_%H-%M")}'
+    exp1_df_results.round(3)
+    print(exp1_df_results)
+    exp1_df_results.to_pickle(filename + '.pkl')
+    exp1_df_results.to_csv(filename + '.csv')
 
 
 if __name__ == "__main__":
-    # export_models()
-    # run_experiment1()
-    run_test()
+    run_experiment1()
