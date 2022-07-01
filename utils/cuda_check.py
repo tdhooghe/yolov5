5ca57a4b50d8076c60b72964d53e54ab4fe2fe22
Thomas <thomas.dhooghe95@gmail.com>
2022-07-01 14:18:00 +0200

    make code work

--- utils/cuda_check.py
+++ utils/cuda_check.py
@@ -1,19 +1,26 @@
 # %%
 import torch
-#import tensorflow as tf
-#%%
+
+
+# import tensorflow as tf
+# %%
 # setting device on GPU if available, else CPU
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print('Using device:', device)
-print()
+def print_gpu_info():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('Using device:', device)
+    print()
 
-# Additional Info when using cuda
-if device.type == 'cuda':
-    print(torch.cuda.get_device_name(0))
-    print('Memory Usage:')
-    print('Allocated:', round(torch.cuda.memory_allocated(0) / 1024 ** 3, 1), 'GB')
-    print('Cached:   ', round(torch.cuda.memory_reserved(0) / 1024 ** 3, 1), 'GB')
+    # Additional Info when using cuda
+    if device.type == 'cuda':
+        print(torch.cuda.get_device_name(0))
+        print('Memory Usage:')
+        print('Allocated:', round(torch.cuda.memory_allocated(0) / 1024 ** 3, 1), 'GB')
+        print('Cached:   ', round(torch.cuda.memory_reserved(0) / 1024 ** 3, 1), 'GB')
 
-#%%
+
+# %%
 #  for a in /sys/bus/pci/devices/*; do echo 0 | sudo tee -a $a/numa_node; done
-print(tf.config.list_physical_devices('GPU'))
+# print(tf.config.list_physical_devices('GPU'))
+
+if __name__ == "__main__":
+    print_gpu_info()
